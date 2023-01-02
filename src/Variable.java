940d60bcf097482a4be2f1cc7e39794136b77e9f
saikumar <saikumargampa5@gmail.com>
2023-01-02 11:22:21 +0530

    adding value of a

---
+++ src/Variable.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Variable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner( System.in );
+        System.out.println("Enter first number : " );
+        int a = sc.nextInt();
+        System.out.println(a);
+    }
+}
