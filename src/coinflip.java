f0d9021c7973419aead5ea9d6b3dcca9eb93806c
saikumar <saikumargampa5@gmail.com>
2023-01-03 08:09:45 +0530

    coins flip

--- src/coinflip.java
+++ src/coinflip.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class coinflip {
+import java.util.Scanner;
+
+public class coinflip {
+    public static void main(String[] args) {
+        System.out.println("Give a number : ");
+        Scanner sc = new Scanner(System.in);
+
+        int headCount = 0;
+        int tailCount = 0;
+        int count = sc.nextInt();
+
+        for (int i=0;i<count;i++) {
+            double random = Math.random();
+            if (random < 0.5)
+                tailCount++;
+            else
+                headCount++;
+        }
+        System.out.println("percentage of tails (" + tailCount + "): " + (tailCount * 100.0 / (headCount + tailCount)) );
+        System.out.println("percentage of heads (" + headCount + "): : " + (headCount * 100.0 / (headCount + tailCount)) );
+    }
 }
