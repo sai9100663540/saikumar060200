a9426f066506c5382bfdf1f4d5a1980b70bc598f
saikumar <saikumargampa5@gmail.com>
2023-01-03 23:38:31 +0530

    Powerof2

---
+++ src/LineComparison/Powerof2.java
@@ -0,0 +1,12 @@
+package LineComparison;
+
+public class Powerof2 {
+    public static void main(String[] args) {
+
+        long l = (long) Math.pow(2, 1000);
+
+        System.out.println(l);
+
+    }
+}
+
