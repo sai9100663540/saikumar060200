a38b3941498213c49e3b502afc3cf1f97aceed6d
saikumar <saikumargampa5@gmail.com>
2023-01-04 07:59:59 +0530

    QuotientAndRemainder

---
+++ src/LineComparison/QuotientAndRemainder.java
@@ -0,0 +1,16 @@
+package LineComparison;
+
+public class QuotientAndRemainder {
+
+    public static void main(String[] args) {
+
+        int dividend = 24, divisor = 6;
+
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        System.out.println("Quotient = " + quotient);
+        System.out.println("Remainder = " + remainder);
+
+    }
+}
